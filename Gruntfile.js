e771fa3fd254c92c0b02fe88bd4fa0f20ce7b981
Gage Choat <gagechoat@gmail.com>
2014-11-10 10:58:57 -0800

    snowflake updates, update npm dependies

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,7 +75,7 @@ module.exports = function (grunt) {
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
+        hostname: '10.1.10.97' //'localhost'
       },
       livereload: {
         options: {
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,28 +1,3 @@
-/*
-    
-    The `impress()` function also gives you access to the API that controls the presentation.
-    
-    Just store the result of the call:
-    
-        var api = impress();
-    
-    and you will get three functions you can call:
-    
-        `api.init()` - initializes the presentation,
-        `api.next()` - moves to next step of the presentation,
-        `api.prev()` - moves to previous step of the presentation,
-        `api.goto( idx | id | element, [duration] )` - moves the presentation to the step given by its index number
-                id or the DOM element; second parameter can be used to define duration of the transition in ms,
-                but it's optional - if not provided default transition duration for the presentation will be used.
-    
-    You can also simply call `impress()` again to get the API, so `impress().next()` is also allowed.
-    Don't worry, it wont initialize the presentation again.
-    
-    For some example uses of this API check the last part of the source of impress.js where the API
-    is used in event handlers.
-    
-*/
-
 var ij = impress();
 
 $(document).ready(function() {
